9caf9f3e8c2429bf5d42f6bb0d40e9fcb2677e03
FireFull45 <firefull2945@gmail.com>
2023-06-08 15:06:16 +0200

    add Dockerfile

---
+++ Dockerfile
@@ -0,0 +1,31 @@
+FROM node:slim
+
+# We don't need the standalone Chromium
+ENV PUPPETEER_SKIP_CHROMIUM_DOWNLOAD true
+
+# Install Google Chrome Stable and fonts
+# Note: this installs the necessary libs to make the browser work with Puppeteer.
+RUN apt-get update && apt-get install gnupg wget -y && \
+  wget --quiet --output-document=- https://dl-ssl.google.com/linux/linux_signing_key.pub | gpg --dearmor > /etc/apt/trusted.gpg.d/google-archive.gpg && \
+  sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list' && \
+  apt-get update && \
+  apt-get install google-chrome-stable -y --no-install-recommends && \
+  rm -rf /var/lib/apt/lists/*
+
+# Répertoire de travail
+WORKDIR /app
+
+# Copier package.json et package-lock.json (si disponible)
+COPY package*.json ./
+
+# Installation des dépendances du projet
+RUN npm install
+
+# Copie des fichiers sources du projet
+COPY . .
+
+# Port d'écoute de l'application
+EXPOSE 3000
+
+# Commande pour démarrer l'application
+CMD [ "npm", "start" ]
--- server.js
+++ server.js
@@ -12,7 +12,11 @@ app.use(bodyParser.json());
 app.set('view engine', 'ejs');
 
 async function generatePdfFromHtml(html) {
-  const browser = await puppeteer.launch();
+  const browser = await puppeteer.launch({
+    executablePath: '/usr/bin/google-chrome',
+    headless: true,
+    args: ['--no-sandbox', '--disable-setuid-sandbox']
+  });
   const page = await browser.newPage();
   await page.setContent(html, { waitUntil: 'networkidle2' });
   const pdf = await page.pdf({ format: 'A4', printBackground: true });
@@ -29,9 +33,9 @@ app.post('/generate-pdf', async function (req, res) {
 
   // Render your EJS template with the form data
   let html = await new Promise((resolve, reject) => {
-    ejs.renderFile(path.join(__dirname, 'public', 'ticket.ejs'), formData, {}, function(err, str){
-        if(err) return reject(err);
-        resolve(str);
+    ejs.renderFile(path.join(__dirname, 'public', 'ticket.ejs'), formData, {}, function (err, str) {
+      if (err) return reject(err);
+      resolve(str);
     });
   });
 
@@ -39,7 +43,7 @@ app.post('/generate-pdf', async function (req, res) {
   const pdf = await generatePdfFromHtml(html);
 
   // Send the PDF as the response
-  res.setHeader('Content-Disposition', 'attachment; filename=ticket-'+formData.model+'.pdf');
+  res.setHeader('Content-Disposition', 'attachment; filename=ticket-' + formData.model + '.pdf');
   res.contentType('application/pdf');
   res.send(pdf);
 });
